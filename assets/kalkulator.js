1e388f980d1edd4129d2c8d4ee5848d183b1dca7
Alberto0150 <albertosanjaya2@gmail.com>
2020-07-28 14:18:10 +0700

    clear button

--- assets/kalkulator.js
+++ assets/kalkulator.js
@@ -1,12 +1,12 @@
 const calculator = {
-    displayNumber: '0',
-    operator:null,
-    firstNumber:null,
-    waitingForSecondNumber:false
+    displayNumber : '0',
+    operator : null,
+    firstNumber : null,
+    waitingForSecondNumber : false
 };
 
 function updateDisplay() {
-    document.querySelector("displayNumber").innerText = calculator.displayNumber;
+    document.querySelector("#displayNumber").innerText = calculator.displayNumber;
 }
 
 function clearCalculator()
@@ -18,16 +18,29 @@ function clearCalculator()
 }
 
 function inputDigit(digit) {
-    calculator.displayNumber +=digit;
+    if(calculator.displayNumber === '0')
+    {
+        calculator.displayNumber = digit;
+    }
+    else
+    {
+        calculator.displayNumber +=digit;
+    }
 }
 
 const buttons = document.querySelectorAll(".button");
 
 for (let button of buttons)
 {
-    button.addEventListener('click',function(event) {
+    button.addEventListener('click',function(event) 
+    {
         const target = event.target;
-
+        
+        if(target.classList.contains('clear')) {
+            clearCalculator();
+            updateDisplay();
+            return;
+        }
         inputDigit(target.innerText);
         updateDisplay();
     });
